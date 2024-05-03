20f55520a4e47036b0626c52c53df1ca9a1a6c86
David Augusto <davidribeiro086@gmail.com>
2024-05-03 13:37:28 -0300

    chore: config providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app'
+
+import { ThemeProvider } from 'styled-components'
+
+import { Theme } from '@radix-ui/themes'
+
+import '@radix-ui/themes/styles.css'
+import GlobalStyles from '@styles/global'
+import { defaultTheme } from '@styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Theme accentColor="indigo">
+        <Component {...pageProps} />
+      </Theme>
+      <GlobalStyles />
+    </ThemeProvider>
+  )
 }
