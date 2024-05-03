3c341452918840eb9206975c1fac817d0892087a
David Augusto <davidribeiro086@gmail.com>
2024-05-03 14:00:24 -0300

    fix: eslint errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,13 @@
 import type { AppProps } from 'next/app'
 
-
 import { Theme } from '@radix-ui/themes'
 
 import '@radix-ui/themes/styles.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-      <Theme accentColor="indigo">
-        <Component {...pageProps} />
-      </Theme>
+    <Theme accentColor="indigo">
+      <Component {...pageProps} />
+    </Theme>
   )
 }
